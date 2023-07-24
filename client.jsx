1fe42acecfbc8be9095553edda1b84603fa2b237
Junsus <119837015+Junsu930@users.noreply.github.com>
2023-07-24 23:01:26 +0900

    common

---
+++ client.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import ResponseCheck from './ResposeCheck';
+
+ReactDom.createRoot(document.getElementById("root")).render(<ResponseCheck/>);
