8d0f72dc0be9f04dc46550530962d150c14144ae
Junsus <119837015+Junsu930@users.noreply.github.com>
2023-07-26 18:28:58 +0900

    dg

--- ResposeCheck.jsx
+++ ResposeCheck.jsx
@@ -7,8 +7,44 @@ class ResponseCheck extends Component{
 			result : [],
 	};
 
-	onClickScreen = () =>{
+	timeout;
+	startTime;
+	endTime;
 
+	onClickScreen = () =>{
+		const {state, message, result} = this.state;
+		if(state=='waiting'){
+			this.setState({
+				state : 'ready',
+				message: '초록색이 되면 클릭하세요',
+			});
+			this.timeout = setTimeout(()=>{
+				this.setState({
+					state: 'now',
+					message: '지금 클릭하세요'
+				});
+				this.startTime = new Date();
+			}, Math.floor(Math.random() * 1000) + 2000) // 2~3초 랜덤;
+		}
+		if(state == 'ready'){
+			this.setState({
+				state: 'waiting',
+				message: '너무 성급하시네요 초록색일 때 클릭하세요',
+			});
+			clearTimeout(this.timeout);
+		}
+
+		if(state=='now'){
+			this.endTime = new Date();
+			this.setState((prevState)=>{
+				return{
+					state: 'waiting',
+					message: '클릭해서 시작하세요',
+					result: [...prevState.result, this.endTime - this.startTime],
+				}
+			});
+			console.log(this.endTime - this.startTime);
+		}
 	}
 
 	renderAverage = () =>{
