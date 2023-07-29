f254b47b0882585239e7e479b46fb8b74f7a3c9a
Junsus <119837015+Junsu930@users.noreply.github.com>
2023-07-29 21:11:17 +0900

    rcp

---
+++ RCP/client.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import RCP from './rcp';
+
+ReactDom.createRoot(document.getElementById("root")).render(<RCP/>);
---
+++ RCP/rcp.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class RCP extends Component{
+  render(){
+    return(
+      <>
+      </>
+    );
+  }
+};
+
+export default RCP;
